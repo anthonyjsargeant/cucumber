f172239ce5e27c37039b2dd40902a2e3b1e1ea4e
Anthony Sargeant <anthony.sargeant@sky.uk>
2019-10-31 13:26:38 +0000

    Changed the style to Google JavaScript Style, and removed the "Standard" linter

--- src/bank.js
+++ src/bank.js
@@ -1,54 +1,57 @@
-const BankAccount = require('./bankAccount')
+const BankAccount = require('./bankAccount');
 
 module.exports = class Bank {
-  constructor () {
-    this._bankAccounts = []
+  constructor() {
+    this._bankAccounts = [];
   }
 
-  get bankAccounts () {
-    return this._bankAccounts
+  get bankAccounts() {
+    return this._bankAccounts;
   }
 
-  set bankAccounts (value) {
-    this._bankAccounts = value
+  set bankAccounts(value) {
+    this._bankAccounts = value;
   }
 
-  getAccountByAccountNumber (accountNumber) {
-    const bankAccount = this.bankAccounts.find(account => account.accountNumber === accountNumber)
+  getAccountByAccountNumber(accountNumber) {
+    const bankAccount = this.bankAccounts.find(
+        account => account.accountNumber === accountNumber);
 
     if (bankAccount === undefined) {
-      throw new Error(`Account with Account Number ${accountNumber} not found`)
+      throw new Error(`Account with Account Number ${accountNumber} not found`);
     } else {
-      return bankAccount
+      return bankAccount;
     }
   };
 
-  createAccount (accountNumber, firstName, lastName, emailAddress, initialDeposit) {
-    const bankAccount = new BankAccount(accountNumber, firstName, lastName, emailAddress)
-    bankAccount.deposit(initialDeposit)
-    this.bankAccounts.push(bankAccount)
+  createAccount(
+      accountNumber, firstName, lastName, emailAddress, initialDeposit) {
+    const bankAccount = new BankAccount(accountNumber, firstName, lastName,
+        emailAddress);
+    bankAccount.deposit(initialDeposit);
+    this.bankAccounts.push(bankAccount);
   };
 
-  makeDeposit (accountNumber, depositAmount) {
+  makeDeposit(accountNumber, depositAmount) {
     if (depositAmount === 0) {
-      throw new Error('You cannot deposit a zero amount')
+      throw new Error('You cannot deposit a zero amount');
     } else if (depositAmount < 0) {
-      throw new Error('You cannot deposit a negative amount')
+      throw new Error('You cannot deposit a negative amount');
     }
-    const bankAccount = this.getAccountByAccountNumber(accountNumber)
-    bankAccount.deposit(depositAmount)
+    const bankAccount = this.getAccountByAccountNumber(accountNumber);
+    bankAccount.deposit(depositAmount);
   };
 
-  getCurrentBalance (accountNumber) {
-    const bankAccount = this.getAccountByAccountNumber(accountNumber)
-    return bankAccount.currentBalance
+  getCurrentBalance(accountNumber) {
+    const bankAccount = this.getAccountByAccountNumber(accountNumber);
+    return bankAccount.currentBalance;
   };
 
-  withdraw (accountNumber, withdrawalAmount) {
+  withdraw(accountNumber, withdrawalAmount) {
     if (withdrawalAmount < 0) {
-      throw new Error('You cannot withdraw a negative amount')
+      throw new Error('You cannot withdraw a negative amount');
     }
-    const bankAccount = this.getAccountByAccountNumber(accountNumber)
-    bankAccount.withdraw(withdrawalAmount)
+    const bankAccount = this.getAccountByAccountNumber(accountNumber);
+    bankAccount.withdraw(withdrawalAmount);
   }
-}
+};
--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,53 +1,53 @@
 module.exports = class BankAccount {
-  constructor (accountNumber, firstName, lastName, emailAddress) {
-    this._accountNumber = accountNumber
-    this._firstName = firstName
-    this._lastName = lastName
-    this._emailAddress = emailAddress
-    this._balance = 0
+  constructor(accountNumber, firstName, lastName, emailAddress) {
+    this._accountNumber = accountNumber;
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._emailAddress = emailAddress;
+    this._balance = 0;
   }
 
-  get accountNumber () {
-    return this._accountNumber
+  get accountNumber() {
+    return this._accountNumber;
   }
 
-  set accountNumber (accountNumber) {
-    this._accountNumber = accountNumber
+  set accountNumber(accountNumber) {
+    this._accountNumber = accountNumber;
   }
 
-  get firstName () {
-    return this._firstName
+  get firstName() {
+    return this._firstName;
   }
 
-  set firstName (firstName) {
-    this._firstName = firstName
+  set firstName(firstName) {
+    this._firstName = firstName;
   }
 
-  get lastName () {
-    return this._lastName
+  get lastName() {
+    return this._lastName;
   }
 
-  set lastName (lastName) {
-    this._lastName = lastName
+  set lastName(lastName) {
+    this._lastName = lastName;
   }
 
-  get emailAddress () {
-    return this._emailAddress
+  get emailAddress() {
+    return this._emailAddress;
   }
 
-  set emailAddress (emailAddress) {
-    this._emailAddress = emailAddress
+  set emailAddress(emailAddress) {
+    this._emailAddress = emailAddress;
   }
 
-  get currentBalance () {
-    return this._balance
+  get currentBalance() {
+    return this._balance;
   }
 
-  deposit (amount) {
-    this._balance += amount
+  deposit(amount) {
+    this._balance += amount;
   }
 
-  withdraw (amount) {
-    this._balance -= amount
+  withdraw(amount) {
+    this._balance -= amount;
   }
-}
+};
